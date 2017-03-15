e78d21755a26bf54f86e1b523a9cb310724c42ca
Maksym Pugach <pugach.m@gmail.com>
2017-03-15 14:32:38 +0200

    [#211] remove country input

--- frontend/app/components/oracle-form.js
+++ frontend/app/components/oracle-form.js
@@ -3,16 +3,12 @@ import Ember from 'ember';
 const {
   Component,
   computed: {
-    sort,
     alias,
   },
 } = Ember;
 
 export default Component.extend({
-  countrySorting: ['name:asc'],
-
   sexes: alias('selectableData.sexes'),
-  countries: sort('selectableData.countries', 'countrySorting'),
 
   actions: {
     askOracle(oracleRequest) {
